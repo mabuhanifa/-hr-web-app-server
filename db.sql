d355bed2cadc8979c78b4e0224d8ae4c265c77ff
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-31 14:02:25 +0600

    table employee

--- db.sql
+++ db.sql
@@ -5,3 +5,14 @@ CREATE TABLE users (
     password VARCHAR NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE
 );
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    img TEXT,
+    department TEXT,
+    leaveStatus TEXT,
+    isAdmin BOOLEAN NOT NULL DEFAULT FALSE,
+    passReset BOOLEAN NOT NULL DEFAULT FALSE
+);
