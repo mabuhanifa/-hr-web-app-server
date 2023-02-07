3df7119c224d7e7b01bed5653b761b1ef3f09059
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-07 23:13:56 +0600

    functionality updated

--- server.js
+++ server.js
@@ -22,10 +22,30 @@ let transporter = nodemailer.createTransport({
 app.get("/users/:email", async (req, res) => {
   try {
     const { email } = req.params;
-    console.log(email);
     const user = await pool.query("SELECT * FROM employee WHERE email = $1", [
       email,
     ]);
+
+    res.status(201).json({
+      message: `success`,
+      data: user.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.put("/user/:email", async (req, res) => {
+  try {
+    const { email } = req.params;
+    const { name, img, password, department, leavestatus, isadmin, passreset } =
+      req.body.body;
+
+    const user = await pool.query(
+      "UPDATE employee SET name=$1, img=$2, password=$3, department=$4, leavestatus=$5, isadmin=$6, passreset=$7 WHERE email=$8 RETURNING *",
+      [name, img, password, department, leavestatus, isadmin, passreset, email]
+    );
+
     res.status(201).json({
       message: `success`,
       data: user.rows,
@@ -62,8 +82,17 @@ app.post("/login/:email", async (req, res) => {
   }
 });
 
-app.post("/signup", async (req, res) => {
-  // create reusable transporter object using the default SMTP transport
+app.get("/users", async (req, res) => {
+  try {
+    const user = await pool.query("SELECT * FROM employee");
+
+    res.status(201).json({
+      message: `success`,
+      data: user.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.post("/users", async (req, res) => {
@@ -74,14 +103,14 @@ app.post("/users", async (req, res) => {
       email,
       password,
       department,
-      leaveStatus,
-      isAdmin,
-      passReset,
+      leavestatus,
+      isadmin,
+      passreset,
     } = req.body;
 
     const newEmployee = await pool.query(
       "INSERT INTO employee (name, img, email, department, leaveStatus, isAdmin, passReset, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-      [name, img, email, department, leaveStatus, isAdmin, passReset, password]
+      [name, img, email, department, leavestatus, isadmin, passreset, password]
     );
     let message = {
       from: "moh.abuhanifa@gmail.com",
